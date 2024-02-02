3a5a5bc7e30cd06cb48be24163dd904ab79730c0
adityasupag1 <123815309+adityasupag1@users.noreply.github.com>
2024-02-02 19:02:16 +0530

    top 50 interview topic wise dsa problems

---
+++ java.js
@@ -0,0 +1,84 @@
+// const cards = document.querySelectorAll('.card');
+
+// cards.forEach(card => {
+//   card.addEventListener('click', () => {
+//     window.location.href = "https://www.geeksforgeeks.org/";
+//   });
+// });
+const card1 = document.querySelector('.card1');
+card1.addEventListener('click', () => {
+    window.open("https://www.geeksforgeeks.org/top-50-array-coding-problems-for-interviews/", "_blank");
+  });
+
+
+const card2 = document.querySelector('.card2');
+card2.addEventListener('click', () => {
+    window.open("https://www.geeksforgeeks.org/top-50-interview-problems-on-recursion-algorithm/", "_blank");
+  });
+
+const card3 = document.querySelector('.card3');
+card3.addEventListener('click', () => {
+    window.open("https://www.geeksforgeeks.org/top-50-string-coding-problems-for-interviews/", "_blank");
+  });
+
+  const card4 = document.querySelector('.card4');
+  card4.addEventListener('click', () => {
+      window.open("", "_blank");
+    });
+
+const card5 = document.querySelector('.card5');
+card5.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+const card6 = document.querySelector('.card6');
+card6.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+const card7 = document.querySelector('.card7');
+card7.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+const card8 = document.querySelector('.card8');
+card8.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+const card9 = document.querySelector('.card9');
+card9.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+const card10 = document.querySelector('.card10');
+card10.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+const card11 = document.querySelector('.card11');
+card11.addEventListener('click', () => {
+    window.open("https://www.geeksforgeeks.org/top-50-problems-on-stack-data-structure-asked-in-interviews/", "_blank");
+  });
+const card12 = document.querySelector('.card12');
+card12.addEventListener('click', () => {
+    window.open("", "_blank");
+  });
+  const card13 = document.querySelector('.card13');
+  card13.addEventListener('click', () => {
+      window.open("https://www.geeksforgeeks.org/top-50-graph-coding-problems-for-interviews/", "_blank");
+    });
+  
+
+  const card14 = document.querySelector('.card14');
+  card14.addEventListener('click', () => {
+      window.open("https://www.geeksforgeeks.org/top-50-tree-coding-problems-for-interviews/", "_blank");
+    });
+    
+    const card15 = document.querySelector('.card15');
+    card15.addEventListener('click', () => {
+        window.open("https://www.geeksforgeeks.org/top-50-dynamic-programming-coding-problems-for-interviews/", "_blank");
+      });
+    const card16 = document.querySelector('.card16');
+    card16.addEventListener('click', () => {
+        window.open("https://www.geeksforgeeks.org/mysql-interview-questions/", "_blank");
+      });
+    const card18 = document.querySelector('.card18');
+    card18.addEventListener('click', () => {
+        window.open("https://www.geeksforgeeks.org/cpp-interview-questions/", "_blank");
+      });
+
