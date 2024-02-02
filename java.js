dcc0233cb160b5db306c9c4e603b93947d34d61d
adityasupag1 <123815309+adityasupag1@users.noreply.github.com>
2024-02-02 19:28:17 +0530

    Update java.js
    updated

--- java.js
+++ java.js
@@ -28,15 +28,15 @@ card3.addEventListener('click', () => {
 
 const card5 = document.querySelector('.card5');
 card5.addEventListener('click', () => {
-    window.open("", "_blank");
+    window.open("https://www.geeksforgeeks.org/top-20-linked-list-interview-question/", "_blank");
   });
 const card6 = document.querySelector('.card6');
 card6.addEventListener('click', () => {
-    window.open("", "_blank");
+    window.open("https://www.geeksforgeeks.org/most-asked-binary-search-interview-questions/", "_blank");
   });
 const card7 = document.querySelector('.card7');
 card7.addEventListener('click', () => {
-    window.open("", "_blank");
+    window.open("https://igotanoffer.com/blogs/tech/greedy-algorithm-interview-questions", "_blank");
   });
 const card8 = document.querySelector('.card8');
 card8.addEventListener('click', () => {
@@ -48,7 +48,7 @@ card9.addEventListener('click', () => {
   });
 const card10 = document.querySelector('.card10');
 card10.addEventListener('click', () => {
-    window.open("", "_blank");
+    window.open("https://www.geeksforgeeks.org/top-50-problems-on-heap-data-structure-asked-in-interviews/", "_blank");
   });
 const card11 = document.querySelector('.card11');
 card11.addEventListener('click', () => {
@@ -80,5 +80,9 @@ card12.addEventListener('click', () => {
     const card18 = document.querySelector('.card18');
     card18.addEventListener('click', () => {
         window.open("https://www.geeksforgeeks.org/cpp-interview-questions/", "_blank");
+      });
+    const card17 = document.querySelector('.card17');
+    card17.addEventListener('click', () => {
+        window.open("https://www.codingninjas.com/studio/library/oops-interview-questions", "_blank");
       });
 
